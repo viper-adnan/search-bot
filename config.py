3f315a6fb7d085ad7b1d4189829ad7f25a3169f2
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-07-23 14:49:41 +0530

    Update and rename config_sample.env to config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+config = """
+  BOT_TOKEN = "{}"
+  OWNER_ID = {}
+  """.format(
+          os.environ.get("BOT_TOKEN"),
+          os.environ.get("OWNER_ID"))
+
+with open('config.env', 'w') as conf:
+  print(config, file=conf)
+
+drive_index = os.environ.get("DRIVE_INDEX", "")
+
+with open('drive_index.txt', 'w') as di:
+  print(drive_index, file=di)
